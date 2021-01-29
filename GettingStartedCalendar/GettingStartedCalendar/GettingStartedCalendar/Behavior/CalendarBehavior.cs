1f8e88423540770acdc9c93876fab57bae29b6ff
sakithya <sakithya.chidambaram@syncfusion.com>
2021-01-29 11:49:55 +0530

    samples updated

---
+++ GettingStartedCalendar/GettingStartedCalendar/GettingStartedCalendar/Behavior/CalendarBehavior.cs
@@ -0,0 +1,34 @@
+﻿using Syncfusion.SfCalendar.XForms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace GettingStartedCalendar
+{
+    public class CalendarBehavior : Behavior<SfCalendar>
+    {
+        SfCalendar calendar;
+        protected override void OnAttachedTo(SfCalendar bindable)
+        {
+            calendar = bindable;
+            calendar.MoveToDate = new DateTime(2018, 12, 31);
+            calendar.MinDate = new DateTime(2018, 10, 25);
+            calendar.MaxDate = new DateTime(2018, 12, 31);
+            List<DateTime> black_dates = new List<DateTime>();
+            for (int i = 0; i < 5; i++)
+            {
+                DateTime date = new DateTime(2018, 12, 1 + i);
+                black_dates.Add(date);
+            }
+            calendar.BlackoutDates = black_dates;
+            base.OnAttachedTo(bindable);
+        }
+        protected override void OnDetachingFrom(SfCalendar bindable)
+        {
+            base.OnDetachingFrom(bindable);
+        }
+    }
+}
+
+
